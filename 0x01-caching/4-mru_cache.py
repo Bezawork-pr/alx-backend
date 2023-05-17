12cb9434cb8c5157cabbd637f005e7bc3be0bd41
Bezawork <bezawork-pr@gmail.com>
2023-05-17 04:05:09 -0700

    Try solution with checker

---
+++ 0x01-caching/4-mru_cache.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python3
+"""
+Create a class MRUCache that inherits
+from BaseCaching and is a caching system
+"""
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+def copy_dict(items):
+    """copies a dictionary"""
+    my_dict = {}
+    for keys, values in items.items():
+        my_dict[keys] = 0
+    return my_dict
+
+
+class MRUCache(BaseCaching):
+    """class LRUCache"""
+    def __init__(self):
+        """Instantiate both parent and child class"""
+        super().__init__()
+        self.my_dict = copy_dict(self.cache_data)
+
+    def put(self, key, item):
+        """assign to the dictionary self.cache_data"""
+        cache = self.cache_data
+        if len(cache) == self.MAX_ITEMS and key not in cache:
+            least_assessed = 0
+            least_assessed_key = next(iter(self.cache_data))
+            for keys, values in self.my_dict.items():
+                if least_assessed <= values:
+                    least_assessed = values
+                    least_assessed_key = keys
+            print(self.my_dict)
+            discard = self.cache_data.pop(least_assessed_key)
+            self.my_dict.pop(least_assessed_key)
+            print("DISCARD: {}".format(least_assessed_key))
+        if key and item is not None:
+            new_element = {key: item}
+            self.my_dict.update({key: 0})
+            self.cache_data.update(new_element)
+        self.save_key = key
+
+    def get(self, key):
+        """return the value in self.cache_data linked to key"""
+        try:
+            self.my_dict[key] += 1
+            print(self.my_dict)
+            return self.cache_data[key]
+        except Exception as NotFount:
+            return None
