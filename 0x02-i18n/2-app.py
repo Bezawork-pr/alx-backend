646831fcb4479deb33c4201e186c5bee5e823bfe
Bezawork <bezawork-pr@gmail.com>
2023-05-23 03:03:32 -0700

    Create function get_locale

---
+++ 0x02-i18n/2-app.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+"""Use flask_babel"""
+from flask_babel import Babel
+from flask import Flask, render_template, request
+
+
+app = Flask(__name__)
+babel = Babel(app)
+
+
+def get_locale():
+    """ The decorated function is invoked
+    for each request to select a
+    language translation to
+    use for that request"""
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+babel.init_app(app, locale_selector=get_locale)
+
+class Config(object):
+    """To keep track of the list
+    of supported languages"""
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app.config.from_object(Config)
+
+
+@app.route('/')
+def index():
+    """Greet page visitors"""
+    return render_template('2-index.html')
