6714f3de8823e4fc47bfdc0cf2b53ec2a6fb458a
Bezawork <bezawork-pr@gmail.com>
2023-05-23 01:54:58 -0700

    Upload first draft and test visual studio

---
+++ 0x02-i18n/app.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""setup a basic Flask app"""
+from flask import Flask, render_template
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    """Greet page visitors"""
+    return render_template('index.html')
+
